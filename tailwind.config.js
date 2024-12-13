9687f778c1e1d64957b64f1ea7922699d5b1b71b
Rikughosh <125813337+Rikughosh@users.noreply.github.com>
2024-12-13 20:10:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a365d',
+        secondary: '#2b6cb0',
+      },
+    },
+  },
+  plugins: [],
+}
